bf692e9caa7ed802ad51cfb597de9601adddebe0
kniklo <kateryna.stepanova@nure.ua>
2025-06-19 15:47:16 +0300

    dmapp fixed

--- dmapp.py
+++ dmapp.py
@@ -1,11 +1,4 @@
-# from app import app
-#
-# if __name__ == '__main__':
-#     app.run(debug=False, port=8000, host="0.0.0.0")
-
-from app import create_app
-
-app = create_app()
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=False, port=8000, host="0.0.0.0")
